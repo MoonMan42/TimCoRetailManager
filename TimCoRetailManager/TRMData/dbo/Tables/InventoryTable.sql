cc076d174cb632c207f608e1b943ed23890880ab
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-09-22 22:45:10 -0400

    Setup DB tables and updated the database.

---
+++ TimCoRetailManager/TRMData/dbo/Tables/InventoryTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[InventoryTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NULL DEFAULT getutcdate()
+)
