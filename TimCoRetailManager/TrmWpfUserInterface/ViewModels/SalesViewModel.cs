fb5c0c973cd625dcec85742abab83a042e84795c
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-10-07 02:35:54 -0400

    Schafold out the Sales View and ViewModel

---
+++ TimCoRetailManager/TrmWpfUserInterface/ViewModels/SalesViewModel.cs
@@ -0,0 +1,139 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrmWpfUserInterface.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+
+        // total
+        public string SubTotal
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+
+        }
+
+        public string Total
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+
+        }
+
+        public string Tax
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+
+        }
+
+
+        // buttons 
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+                // check if item is selected 
+                // and that quantity has value
+
+                return output;
+            }
+
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+                // check if item is selected 
+
+                return output;
+            }
+
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+                // check if something is in the cart
+
+                return output;
+            }
+
+        }
+
+        public void CheckOut()
+        {
+
+        }
+
+    }
+}
